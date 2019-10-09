8cf4e71e976369dc81b36ed9d3fbf7a51f45ce40
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 18:08:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,19 +2,19 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "",
-        :age => ""
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
         :name => "",
-        :age => ""
+        :age => "54"
         },
       :hero => {
         :name => "",
         :age => "",
         :status => ""
         },
-      :hero_friends => []
+      :hero_friends => [:name, :age, :status]
    },
    :capulet => {
       :patriarch => {
@@ -30,7 +30,7 @@ def third_challenge
         :age => "",
         :status => ""
         },
-      :heroine_friends => []
+      :heroine_friends => [:name, :age, :status]
    }
   }
 
